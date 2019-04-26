451d81b63561e66dd95626c5a4b3786f65e53a10
rubenaj <rubenaj88@gmail.com>
2019-04-26 13:51:13 +0200

    app claculator ionic

--- AngularBootstrap/javascriptCalculadora/calculator.js
+++ AngularBootstrap/javascriptCalculadora/calculator.js
@@ -1,18 +1,30 @@
+function onLoad() {
+    document.addEventListener("ionChange", setStyle);
+    setStyle();
+}
+function setStyle() {
+    document.querySelectorAll("ion-content ion-button").forEach(function(b) {
+        b.expand = "block";
+        b.strong = "true";
+        b.fill = document.getElementById("type").value;
+        b.size = document.getElementById("size").value;
+    });
+}
 function setResult(value) {
-    document.getElementById('result').innerHTML = value;
+    document.getElementById("result").innerHTML = value;
 }
 function getResult() {
-    return(document.getElementById('result').innerHTML);
+    return(document.getElementById("result").innerHTML);
 }
-function add(key) { 
+function add(key) {
     var result = getResult();
-    if (result!='0' || isNaN(key)) setResult(result + key);
+    if (result!="0" || isNaN(key)) setResult(result + key);
     else setResult(key);
 }
 function calc() {
-    var result = eval(getResult()); 
+    var result = eval(getResult());
     setResult(result);
 }
-function del() { 
+function del() {
     setResult(0);
 }
