b9538353510a53ba983167e9c7425a6a99f1778f
rubenaj <rubenaj88@gmail.com>
2019-05-08 08:51:32 +0200

    .

--- AngularBootstrap/Ejercicio básico NODE/bdconfig.js
+++ AngularBootstrap/Ejercicio básico NODE/bdconfig.js
@@ -21,3 +21,11 @@ conexion.query("SELECT titulo,precio FROM libros", (error, resultado,
     resultado.forEach((libros) => console.log( libros.titulo,  ":", libros.precio,));
     }
     });
+
+let insertsql="INSERT INTO libros ( cod, isbn, precio, titulo) VALUES ('j1', '1234567','15', ' algun dia')",
+conexion.query(insertsql, (error, resultado, campos) => {
+        if (error)
+        console.log("Error al procesar la inserción");
+        else
+        console.log("Inserción realizada. Nuevo id = ", resultado.insertId);
+        });
